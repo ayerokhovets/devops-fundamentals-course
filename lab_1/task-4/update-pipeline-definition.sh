5a9cca08619a974ef64d16885e8de8737c9d7b57
Alexander Yerokhovets <aliaksandr_yerakhavets@epam.com>
2022-08-15 00:54:29 +0400

    (lab1)task4 was updated

--- lab_1/task-4/update-pipeline-definition.sh
+++ lab_1/task-4/update-pipeline-definition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 defaultPipeline="pipeline"
-defaultBranchName="develop"
+defaultBranch="main"
 
 pipelineJsonCopy="pipeline-$(date +'%m-%d-%Y').json"
 
@@ -13,9 +13,9 @@ end=$'\e[0m'
 ## Check if jq is installed
 checkJQ() {
   type jq >/dev/null 2>/dev/null
-  jqCheckResult=$?
+  checkJqResult=$? # $? is the result
 
-  if [ "$jqCheckResult" -ne 0 ]; then
+  if [ "$checkJqResult" -ne 0 ]; then
     printf "  ${red}'jq' not found! (json parser)\n${end}"
     printf "  MacOS Installation: brew install jq\n"
     printf "  Ubuntu Installation: sudo apt install jq\n"
@@ -25,6 +25,7 @@ checkJQ() {
   fi
 }
 
+## Check if the first param was provided. It should be a path to the pipeline.json.
 checkFirstParam() {
   local command=$1
   ## -z tests if the expansion of "$1" is a null string
@@ -36,17 +37,39 @@ checkFirstParam() {
 
 pipelineJson=$1
 
-# Perform checks:
+## Perform checks
 checkJQ
 checkFirstParam $pipelineJson
 
+## Copy json
+cat $pipelineJson > $pipelineJsonCopy
+
 ## Remove metadata
 echo "Removing metadata..."
-jq 'del(.metadata)' "$pipelineJson" > tmp.$$.json && mv tmp.$$.json "$pipelineJsonCopy"
+jq 'del(.metadata)' "$pipelineJsonCopy" > tmp.$$.json && mv tmp.$$.json "$pipelineJsonCopy"
+## Increment version
+echo "Incrementing version..."
+jq '.pipeline.version +=1' "$pipelineJsonCopy" > tmp.json && mv tmp.json "$pipelineJsonCopy"
 
 ## Perform only metadata delete and version upgrade if there is only one param provided.
 if [ "$#" -eq "1" ]; then
   exit 0
 fi
+
+## Get the options (only short ones)
+while getopts b:c:o: option; do
+   case $option in
+      b) branch=$OPTARG;; # Branch
+      c) configuration=$OPTARG;; # Configuration
+      o) owner=$OPTARG;; # Owner
+      \?) # Invalid option
+        echo "Error: Invalid option"
+        exit;;
+   esac
+done
+
+echo "Branch: $branch"
+echo "Configuration: $configuration"
+echo "Owner: $owner"
 
 exit 0
